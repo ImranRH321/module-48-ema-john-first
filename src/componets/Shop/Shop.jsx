08f255eebe74b94e46a8b31958780a6afe6fbdb8
ImranRH321 <imranrh247@gmail.com>
2022-05-17 04:11:38 +0600

    eme-john-part-1

---
+++ src/componets/Shop/Shop.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
+import "./Shop.css";
+
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const [cart , setCart] = useState([])
+
+  useEffect(() => {
+    fetch("products.json")
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  }, []);
+
+
+  const handleAddToCart  = product => {
+    const newCart = [...cart, product]
+    setCart(newCart)
+}
+
+  return (
+    <div className="shop-container">
+
+      <div className="products-container">
+        {products.map(product => (
+          <Product handleAddToCart={handleAddToCart} product={product}></Product>
+        ))}
+      </div>
+
+      <div className="cart-container">
+        <h2>Order summary </h2>
+        <p>Selected Items: {cart.length}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
